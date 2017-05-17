aa5cfa73e3731ff74cfbca6aed65064709c1423b
aparkhomchik <parkhomchik@gmail.com>
2017-05-17 11:47:10 +0600

    добавил обработку формы

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"html/template"
+	"fmt"
+	"strconv"
 )
 
 type page struct {
@@ -10,16 +12,46 @@ type page struct {
 	Msg string
 }
 
-func hello(w http.ResponseWriter, req *http.Request) {
+func index(w http.ResponseWriter, req *http.Request) {
 
 	w.Header().Set("Content-type", "text/html")
     t, _ := template.ParseFiles("tmpl/index.html")
     t.Execute(w, &page{Title: "Моментальные переводы денег онлайн между картами любых банков Казахстана Card2Card"})	
 }
 
+func p2p(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("method:", r.Method)
+    if r.Method == "GET" {
+        t, _ := template.ParseFiles("tmpl/index.html")
+        t.Execute(w, &page{Title: "Моментальные переводы денег онлайн между картами любых банков Казахстана Card2Card"})
+    } else {
+        r.ParseForm()
+		senderCard := ""
+		for i := 1; i <=4; i++ {
+			sc := r.Form["sender_card_num_"+strconv.Itoa(i)]
+			senderCard += sc[0]
+		}
+
+		//senderCard := r.Form["sender_card_num_1"]
+		//senderCard = append(senderCard, r.Form["sender_card_num_2"])
+
+		/*senderCard1 := r.Form["sender_card_num_1"]
+		senderCard2 := r.Form["sender_card_num_2"]
+		senderCard3 := r.Form["sender_card_num_3"]
+		senderCard4 := r.Form["sender_card_num_4"]
+
+		senderCard := senderCard1[0] + senderCard2[0] + senderCard3[0] + senderCard4[0]*/
+
+
+        fmt.Println("sender card:", senderCard)
+    }
+}
+
 func main() {
 	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./img/"))))
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css/"))))
-    http.HandleFunc("/hello", hello)
+    http.HandleFunc("/index", index)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/p2p", p2p)
     http.ListenAndServe(":9000", nil)
 }
